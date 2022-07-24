c29b31ee6d9549a9af440d6161a8baf964f228d4
Allinap <ponomar2505@gmail.com>
2022-07-24 21:41:14 +0300

    new file

---
+++ hw1js/main.js
@@ -0,0 +1,36 @@
+let str1 = 'hello';
+let str2 = 'owu';
+let str3 = 'com' ;
+let str4 = 'ua' ;
+let num1 = 1;
+let num2 = 10;
+let num3 = -999;
+let num4 = 123;
+let num5 = 3.14;
+let num6 = 2.7;
+let num7 = 16;
+let bul1 = true;
+let bul2 = false;
+console.log(str1,str2,str3,str4,num1,num2,num3,num4,num5,
+num6,num7,bul1,bul2);
+alert("str1,str2,str3,str4, \n" + "num1,num2,num3,num4,num5,num6,num7, \n" +
+    "bul1,bul2")
+document.write('str1,str2,str3,str4,num1,num2,num3,num4,num5,\n' +
+    'num6,num7,bul1,bul2');
+let firstName = 'Пономарьова';
+let middleName = 'Аліна';
+let lastName = 'Ігорівна';
+let person = `${firstName} ${middleName} ${lastName}`
+console.log(person);
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a, typeof b, typeof c);
+let name = prompt('ваше ім*я');
+let name2 = prompt('по-батькові');
+let age = prompt('вік');
+console.log(name);
+console.log(name2);
+console.log(age);
+
+
