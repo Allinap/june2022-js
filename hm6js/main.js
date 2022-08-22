7b4e0afc9753b33d457652f72bf3c9f5d6c55cea
Allinap <ponomar2505@gmail.com>
2022-08-22 19:43:34 +0300

    5

---
+++ hm6js/main.js
@@ -0,0 +1,84 @@
+// - Знайти та вивести довижину настипних стрінгових значень
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+
+// let str1 = 'hello world';
+// let str2 = 'lorem ipsum';
+// let str3 = 'javascript is cool';
+// console.log(str1.length, str2.length, str3.length);
+
+
+// - Перевести до великого регістру наступні стрінгові значення
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+// console.log(str1.toUpperCase(), str2.toUpperCase(), str3.toUpperCase());
+
+
+// - Перевести до нижнього регістру настипні стрінгові значення
+// 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+
+// console.log(str1.toLowerCase(), str2.toLowerCase(), str3.toLowerCase());
+
+// - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
+
+
+// - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
+//     let str = 'Ревуть воли як ясла повні';
+// let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
+//
+//     let str = 'Ревуть воли як ясла повні';
+//     const stringToArray = (str) => str.split(' ');
+//     console.log(stringToArray(str));
+//
+// - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
+//
+// let array = [10,8,-7,55,987,-1011,0,1050,0];
+// let strings = array.map(value => value + '')
+// console.log(strings);
+
+
+// - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
+//     let nums = [11,21,3];
+// sortNums(nums,'ascending') // [3,11,21]
+// sortNums(nums,'descending') // [21,11,3]
+//
+// let nums = [11,21,3];
+// const sortNums = (direction,arr) => {
+//     if (direction === 'ascending') {
+//         arr.sort((a, b) => a - b);
+//     } else if (direction === 'descending') {
+//         arr.sort((a, b) => b - a);
+//     }
+//     return arr;
+// }
+// console.log(sortNums('ascending', nums));
+// console.log(sortNums('descending', nums));
+// - є масив
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+// -- відсортувати його за спаданням за monthDuration
+// console.log(coursesAndDurationArray.sort((a, b) =>
+//     b.monthDuration - a.monthDuration))
+// -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+
+// let filter = coursesAndDurationArray.filter(value => value.monthDuration > 5);
+// console.log(filter);
+
+
+// описати колоду карт
+// - знайти піковий туз
+// - всі шістки
+// - всі червоні карти
+// - всі буби
+// - всі трефи від 9 та більше
+//
+//
+// {
+//     cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
+//         value: '', // '6'-'10', 'ace','jack','queen','king','joker'
+//     color:'', // 'red','black'
+// }
